fb1cdfcf4f7b6921c12007657adf76bda92cbf43
Alex Humphreys <alex.humphreys@here.com>
2021-03-22 18:49:29 +0100

    check /invoices and count paid
    Signed-off-by: Alex Humphreys <alex.humphreys@here.com>

--- testclient/src/main.rs
+++ testclient/src/main.rs
@@ -2,6 +2,7 @@ use std::{thread, time};
 use std::env;
 use reqwest::blocking::Client;
 use std::error::Error;
+use serde::{Deserialize, Serialize};
 
 #[macro_use]
 extern crate simple_error;
@@ -16,6 +17,41 @@ impl std::fmt::Display for UnknownErr {
 }
 impl Error for UnknownErr {}
 
+// {"id":1,"customerId":1,"amount":{"value":70.03,"currency":"USD"},"status":"PAID"}
+#[derive(Debug, Serialize, Deserialize)]
+struct Invoice {
+    id: i32,
+    #[serde(rename = "customerId")]
+    customer_id: i32,
+    status: String,
+}
+
+fn invoices() -> Result<Vec<Invoice>, Box<dyn std::error::Error>> {
+    let pay_url = env::var("INVOICEURL").unwrap_or("http://localhost:8000/rest/v1/invoices".to_string());
+
+    let client = Client::new();
+    let res = client.get(pay_url)
+        .send();
+
+    match res {
+        Ok(_res) => {
+            match _res.json::<Vec<Invoice>>() {
+                Ok(inv) => {
+                    Ok(inv)
+                }
+                Err(err) => {
+                    println!("{:#?}", err.to_string());
+                    bail!("couldn't parse response");
+                }
+            }
+        }
+        Err(err) => {
+            println!("{:#?}", err.to_string());
+            bail!("pay request failed");
+        }
+    }
+}
+
 fn pay() -> Result<bool, Box<dyn std::error::Error>> {
     let pay_url = env::var("PAYURL").unwrap_or("http://localhost:8000/rest/v1/invoices/pay".to_string());
 
@@ -25,13 +61,11 @@ fn pay() -> Result<bool, Box<dyn std::error::Error>> {
 
     match res {
         Ok(_res) => {
-            println!("{:#?}", _res);
             match _res.json::<bool>() {
                 Ok(_bool) => {
                     Ok(_bool)
                 }
                 Err(_err) => {
-                    println!("{:#?}", _err.to_string());
                     bail!("couldn't parse response");
                 }
             }
@@ -44,7 +78,7 @@ fn pay() -> Result<bool, Box<dyn std::error::Error>> {
 }
 
 fn retry() -> Result<bool, Box<dyn std::error::Error>> {
-    let max_retries = 5;
+    let max_retries = 30;
     let mut res : Result<bool, Box<dyn std::error::Error>> = Err(Box::new(UnknownErr));
     for i in 1..max_retries {
         let res_ = pay();
@@ -65,11 +99,39 @@ fn retry() -> Result<bool, Box<dyn std::error::Error>> {
     res
 }
 
+fn count_paid(invs : Vec<Invoice>) -> usize {
+    invs.iter().filter(|&x| x.status == "PAID").count()
+}
+
+fn check_invoices(invs : Vec<Invoice>) -> Result<usize, String> {
+    let paid = count_paid(invs);
+    if paid > 2 {
+        Ok(paid)
+    } else {
+        Err("No invoices were paid".to_string())
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::process::exit(match retry() {
-        Ok(bool) => {
-            println!("{:#?}", bool);
-            0
+        Ok(paid) => {
+            if paid {
+                println!("All invoices paid, response: {:#?}", paid);
+                0
+            } else {
+                let inv = invoices()?;
+                let res = check_invoices(inv);
+                match res {
+                    Ok(paid_count) => {
+                        println!("Amount of paid invoices: {:#?}", paid_count);
+                        0
+                    }
+                    Err(err) => {
+                        println!("{:#?}", err);
+                        1
+                    }
+                }
+            }
         }
         Err(err) => {
             println!("{:#?}", err);
